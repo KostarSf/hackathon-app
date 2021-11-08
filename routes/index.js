0c8b434f8b44cbfb4a8dcb483450518f66269aab
KostarSf <kostarsf@gmail.com>
2021-11-08 21:26:03 +0300

    post queries test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hackathon 2021: Dungeon Masters' });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('got a post');
 });
 
 module.exports = router;
