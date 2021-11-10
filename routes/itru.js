11ec18694d7e56e8ce591590a10780038fa7ab50
Maksim Peskov <kostarsf@gmail.com>
2021-11-10 13:18:28 +0300

    start to implement db

--- routes/itru.js
+++ routes/itru.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Database = require('../libs/db');
+var database = new Database();
+
 router.get('/', function(req, res, next) {
   res.render('itru/index', { title: 'ITRU - Главная' });
 });
